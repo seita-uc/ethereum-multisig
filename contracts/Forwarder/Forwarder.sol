8492c7cf4ce68e4294d0375bb61f005f3dc07e88
Seita Uchimura <c201552k@gmail.com>
2019-06-27 13:07:49 +0900

    Forwarderにeventを追加

--- contracts/Forwarder/Forwarder.sol
+++ contracts/Forwarder/Forwarder.sol
@@ -1,62 +1,38 @@
 pragma solidity ^0.5.6;
 import "./IERC20.sol";
-/**
- * Contract that will forward any incoming Ether to the creator of the contract
- */
+
 contract Forwarder {
-  // Address to which any funds sent to this contract will be forwarded
-  address public parentAddress;
-  event ForwarderDeposited(address from, uint value, bytes data);
+    address payable public parentAddress;
+    event ForwarderDeposited(address from, uint256 value, bytes data);
+    event TokenFlushed(address token, uint256 balance);
 
-  /**
-   * Create the contract, and sets the destination address to that of the creator
-   */
-  function Forwarder() public {
-    parentAddress = msg.sender;
-  }
-
-  /**
-   * Modifier that will execute internal code block only if the sender is the parent address
-   */
-  modifier onlyParent {
-    if (msg.sender != parentAddress) {
-      revert();
+    constructor() public {
+        parentAddress = msg.sender;
     }
-    _;
-  }
 
-  /**
-   * Default function; Gets called when Ether is deposited, and forwards it to the parent address
-   */
-  function() public payable {
-    // throws on failure
-    parentAddress.transfer(msg.value);
-    // Fire off the deposited event if we can forward it
-    ForwarderDeposited(msg.sender, msg.value, msg.data);
-  }
-
-  /**
-   * Execute a token transfer of the full balance from the forwarder token to the parent address
-   * @param tokenContractAddress the address of the erc20 token contract
-   */
-  function flushTokens(address tokenContractAddress) public onlyParent {
-    ERC20Interface instance = ERC20Interface(tokenContractAddress);
-    var forwarderAddress = address(this);
-    var forwarderBalance = instance.balanceOf(forwarderAddress);
-    if (forwarderBalance == 0) {
-      return;
+    modifier onlyParent {
+        require(msg.sender == parentAddress, "Only parent can execute this function.");
+        _;
     }
-    if (!instance.transfer(parentAddress, forwarderBalance)) {
-      revert();
-    }
-  }
 
-  /**
-   * It is possible that funds were sent to this address before the contract was deployed.
-   * We can flush those funds to the parent address.
-   */
-  function flush() public {
-    // throws on failure
-    parentAddress.transfer(this.balance);
-  }
+    function() external payable {
+        parentAddress.transfer(msg.value);
+        emit ForwarderDeposited(msg.sender, msg.value, msg.data);
+    }
+
+    function flushTokens(address tokenContractAddress) public onlyParent {
+        IERC20 instance = IERC20(tokenContractAddress);
+        address forwarderAddress = address(this);
+        uint256 forwarderBalance = instance.balanceOf(forwarderAddress);
+        require(forwarderBalance != 0, "Token balance is zero.");
+        require(instance.transfer(parentAddress, forwarderBalance), "Token transfer failed.");
+        emit TokenFlushed(tokenContractAddress, forwarderBalance);
+    }
+
+    //It is possible that funds were sent to this address before the contract was deployed.
+    //We can flush those funds to the parent address.
+    function flush() public {
+        // throws on failure
+        parentAddress.transfer(address(this).balance);
+    }
 }
